bdb31dc02db0ea648a209a9b9321d230c4d8ff8c
nasilver0-0 <migon0329@naver.com>
2023-08-06 01:21:44 +0900

    Week4_나은_quiz
    Week4 quiz 1,2 완료

---
+++ 2023_C_Study/Week4_나은_quiz2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main() {
+	int snail[5][5];
+	int row=0, col=-1 ,k=1, sw = 1, end=4;
+	while (end >= 0) {
+		for (int i = 0; i < end+1; i++) {
+			
+			col += sw;
+			snail[row][col] = k;
+			k++;
+		}
+		for (int i = end; i>0; i--) {
+			
+			row += sw;
+			snail[row][col] = k;
+			k++;
+		}
+		end--;
+		sw = -sw;
+	}
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			printf("%d\t", snail[i][j]);
+		}
+		printf("\n");
+	}
+}
+
