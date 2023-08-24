274497bdebea6e5b4eb0e61f945f09d811615aa9
nasilver0-0 <migon0329@naver.com>
2023-08-24 21:00:17 +0900

    Week7_나은_homework.c
    Week7 homework 수정 끝끝

--- 2023_C_Study/Week7_나은_homework.c
+++ 2023_C_Study/Week7_나은_homework.c
@@ -10,98 +10,120 @@ typedef struct node {
 	struct node* next;
 }Node;
 
-Node* head = NULL;
-Node* tail = NULL;
-
 void print(Node* head);
 
 int main() {
 	int index, index2, new_data,num,i;
-	printf("1,2,3,4,5 중 입력: ");
-	scanf_s("%d", &num);
-	switch (num) {
-	case 1:
-		while (1) {
-			int input;
-			printf("추가할 값: ");
-			scanf_s("%d", &input);
-			if (input <= 0) break;
-			Node* newnode = (Node*)malloc(sizeof(Node));
-			newnode->val = input;
-			if (head == NULL) {
-				tail = newnode;
-				head = newnode;
-				head->next = NULL;
+	Node* head = NULL;
+	Node* prev = NULL;
+	Node* cur = NULL;
+
+	while (1) {
+		printf("1,2,3,4,5 중 입력: ");
+		scanf_s("%d", &num);
+		switch (num) {
+		case 1:
+			while (1) {
+				int input;
+				printf("추가할 값: ");
+				scanf_s("%d", &input);
+				if (input <= 0) {
+					printf("오류\n");
+					break;
+					
+				}
+				Node* newnode = (Node*)malloc(sizeof(Node));
+				newnode->val = input;
+				if (head == NULL) {
+					head = newnode;
+					head->next = NULL;
+				}
+				else {
+					newnode->next = head;
+					head = newnode;
+				}
+				printf("추가 완료\n");
 			}
-			else {
-				newnode->next = head;
-				head = newnode;
+			print(head);
+			break;
+		case 2:
+			printf("삭제할 노드 번호 index: ");
+			scanf_s("%d", &index);
+			if (index < 0) {
+				printf("오류\n");
+				break;
+				
 			}
-			printf("추가 완료\n");
-		}
-		print(head);
-	case 2:
-		printf("삭제할 노드 번호 index: ");
-		scanf_s("%d", &index);
-		if (index < 0) return NULL;
-		Node* prev = NULL;
-		Node* cur = head;
-		for (int j = 0; j < index && cur != NULL; j++) {
-	         cur = cur->next;
-         }
-        if (cur == NULL) return NULL;
-		cur = head;
-		int cur_index = 0;
-		while (cur != NULL) {
-			if (index == 0) {
-				Node* ptr = head;
-				head = head->next;
+		    cur = head;
+			for (int j = 0; j < index && cur != NULL; j++) {
+				cur = cur->next;
+			}
+			if (cur == NULL) {
+				printf("오류\n");
 				break;
 			}
-			prev = cur;
-			cur = cur->next;
-			cur_index++; 
+			cur = head;
+			int cur_index = 0;
+			while (cur != NULL) {
+				if (index == 0) {
+					cur = head;
+					head = head->next;
+					break;
+				}
+				prev = cur;
+				cur = cur->next;
+				cur_index++;
 
-			if (cur_index == index) {
-				prev->next = cur->next;
+				if (cur_index == index) {
+					prev->next = cur->next;
+					break;
+				}
+			}
+			printf("삭제 완료\n");
+			print(head);
+			break;
+		case 3:
+			printf("수정할 노드 index: ");
+			scanf_s("%d", &index2);
+			cur = head;
+			if (index2 < 0) {
+				printf("오류\n");
 				break;
 			}
-		}
-		printf("삭제 완료\n");
-		print(head);
-	case 3:
-		printf("수정할 노드 index: ");
-		scanf_s("%d", &index2);
-		Node* cur2 = head;
-		if (index2 < 0)
-			return NULL;
-		for (int p = 0; p < index2 && cur2 != NULL; p++) {
-			cur2 = cur2->next;
-		}
-		if (cur2 == NULL) return NULL;
-		cur2 = head;
-		printf("수정할 값: ");
-		scanf_s("%d", &new_data);
+				
+			for (int p = 0; p < index2 && cur != NULL; p++) {
+				cur = cur->next;
+			}
+			if (cur == NULL) {
+				printf("오류\n");
+				break;
+			}
+			cur = head;
+			printf("수정할 값: ");
+			scanf_s("%d", &new_data);
 
-		int cur_index2 = 0;
-		while (cur2 != NULL) {
-			if (cur_index2 == index2)
-				cur2->val = new_data;
-			cur2 = cur2->next;
-			cur_index2++;
-		}
-		printf("수정 완료\n");
-	case 4:
-		i = 0;
-        Node* cur3 = head;
-		while (cur3 != NULL) {
-			i++;
-			printf("%d : %d \n", i, cur3->val);
-			cur3 = cur3->next;
-		}
-	case 5:
-		break;
+			int cur_index2 = 0;
+			while (cur != NULL) {
+				if (cur_index2 == index2)
+					cur->val = new_data;
+				cur = cur->next;
+				cur_index2++;
+			}
+			printf("수정 완료\n");
+			break;
+		case 4:
+			i = 0;
+			cur= head;
+			while (cur != NULL) {
+				i++;
+				printf("%d : %d \n", i, cur->val);
+				cur = cur->next;
+			}
+			break;
+		case 5:
+			return 0;
 
+		}
 	}
 }
 
@@ -115,3 +137,5 @@ void print(Node* head) {
 	}
 }
 
+// Node * cur = head; 메인 함수 젤 위에 먼저 초기화
+// cur2, cur3 새롭게 선언하지 말고 cur = head 
